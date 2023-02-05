4a2daf9e8e5367f786b089f27d8aaee7fd6be0f2
Jongseung Lim <penandlim@gmail.com>
2023-02-05 02:57:37 -0500

    Clean up handler file. Remove dummy data

--- reward-monitor-lambda-app/app.ts
+++ reward-monitor-lambda-app/app.ts
@@ -3,14 +3,10 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
-import * as dotenv from 'dotenv';
-import findConfig from 'find-config';
-import * as etherscanApi from 'etherscan-api';
 import { ethers } from 'ethers';
+import { getMinterOwedPutItem } from './src/minterDebtHelper';
 
-dotenv.config({ path: findConfig('.env')?.toString() });
-
-const TABLE_NAME = process.env.TABLE_NAME;
+const TABLE_NAME = process.env.TABLE_NAME as string;
 
 /**
  *
@@ -27,70 +23,29 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     const ddbClient = new DynamoDBClient({});
     const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
-    // Determine the timestamp at the time of the run
-    // Used as PK in DynamoDB
-    const timestamp = new Date().getTime();
-
     // Create an ethers provider
     const mainnetProvider = new ethers.providers.JsonRpcProvider(
         `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
     );
 
-    let api = require('etherscan-api').init('YourApiKey');
-
     // Build the params object for the DynamoDB PutItem command
-    const exampleParams = buildPutItemParams(
-        timestamp,
-        1,
-        '0x1234567890',
-        'Example Contract',
-        'EXAMPLE',
-        '0x1234567890',
-        1,
-        100,
-        100,
-        0,
-    );
+    console.log('Getting minter owed data...');
+
+    const params = await getMinterOwedPutItem(mainnetProvider, TABLE_NAME).catch((err) => {
+        console.error(`Failed to get minter owed data. Error: ${err}`);
+        throw err;
+    });
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(exampleParams.Item),
+        body: JSON.stringify(params.Item),
     };
 
     // Save the data to DynamoDB
-    await ddbDocClient.send(new PutCommand(exampleParams)).catch((err) => {
+    await ddbDocClient.send(new PutCommand(params)).catch((err) => {
         response.statusCode = 500;
         response.body = `Failed to save data to DynamoDB. Error: ${err}`;
     });
 
     return response;
 };
-
-function buildPutItemParams(
-    timestamp: number,
-    chainId: number,
-    contractAddress: string,
-    contractName: string,
-    tokenTicker: string,
-    tokenAddress: string,
-    ratePerSecond: number,
-    currentBalance: number,
-    runwayInSeconds: number,
-    rewardDebt: number,
-) {
-    return {
-        TableName: TABLE_NAME,
-        Item: {
-            Timestamp: timestamp,
-            ChainId: chainId,
-            ContractAddress: contractAddress,
-            ContractName: contractName,
-            RewardTokenTicker: tokenTicker,
-            RewardTokenAddress: tokenAddress,
-            RatePerSecond: ratePerSecond,
-            CurrentBalance: currentBalance,
-            RunwayInSeconds: runwayInSeconds,
-            RewardDebt: rewardDebt,
-        },
-    };
-}
