255f568a37c2d3a9cb161f9de5ec0791fafa5a83
Jongseung Lim <penandlim@gmail.com>
2023-02-06 20:06:58 -0500

    Add function to handle /check

--- reward-monitor-lambda-app/src/api/check.ts
+++ reward-monitor-lambda-app/src/api/check.ts
@@ -1,5 +1,25 @@
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyResult } from 'aws-lambda';
+import { readLatestFromDynamoDB } from '../utils/dynamoDBHelper';
 
+/**
+ * Returns latest element from DynamoDB table
+ * @param ddbDocClient dynamoDB document client to use
+ */
 export const handleCheck = async (ddbDocClient: DynamoDBDocumentClient): Promise<APIGatewayProxyResult> => {
-    
+    const item = await readLatestFromDynamoDB(ddbDocClient, process.env.TABLE_NAME)
+        .then((o) => {
+            if (o.Items && o.Items.length > 0) {
+                return o.Items[0];
+            }
+            throw new Error('No items found in DynamoDB');
+        })
+        .catch((err) => {
+            console.error(`Failed to read latest from DynamoDB. Error: ${err}`);
+            throw err;
+        });
+    return {
+        statusCode: 200,
+        body: JSON.stringify(item),
+    };
 };
