44f6fa9f205ce3c7d7bf6bb1975ee0577ef2bfa9
Michael Daly <dalymike102@gmail.com>
2023-04-17 14:14:21 -0400

    added new function to api for fetching token prices

---
+++ token-price-lambda-app/src/api/check.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+// import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { Config } from '../utils/config';
+
+// https://<api-gateway-url>/prod/getPrices?tokens=0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984,0x514910771AF9Ca656af840dff83E8264EcF986CA
+
+export const handleTokenPrice = async (config: Config, event: APIGatewayProxyEvent) => {
+    const tokenAddresses = event.queryStringParameters?.contractAddresses?.split(',') ?? [];
+    if (tokenAddresses.length === 0) {
+        return {
+            statusCode: 400,
+            body: 'No contract addresses provided',
+        };
+    }
+    const platform = event.queryStringParameters?.platform;
+    if (!platform) {
+        throw new Error('Platform not provided');
+    }
+    // Get new items from CoinGecko
+    const endpoint = `https://api.coingecko.com/api/v3/simple/token_price/${platform}?contract_addresses=${tokenAddresses.join(
+        ',',
+    )}&vs_currencies=usd`;
+
+    try {
+        const response = await fetch(endpoint);
+
+        if (!response.ok) {
+            throw new Error(`Unexpected response: ${response.status} ${response.statusText}`);
+        }
+        // Below includes objects like {"<tokenAddress>": {"usd": 2057.02}
+        const data = await response.json();
+        console.log(data);
+        console.log('stringify: ' + JSON.stringify(data));
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error }),
+        };
+    }
+};
---
+++ token-price-lambda-app/tests/unit/test-handler.test.ts
@@ -0,0 +1,61 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { lambdaHandler } from '../../app';
+
+describe('Unit test for app handler', function () {
+    it('verifies successful response', async () => {
+        const event: APIGatewayProxyEvent = {
+            httpMethod: 'get',
+            body: '',
+            headers: {},
+            isBase64Encoded: false,
+            multiValueHeaders: {},
+            multiValueQueryStringParameters: {},
+            path: '/getPrices',
+            pathParameters: {},
+            queryStringParameters: {
+                contractAddresses:
+                    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+                platform: 'ethereum',
+            },
+            requestContext: {
+                accountId: '123456789012',
+                apiId: '1234',
+                authorizer: {},
+                httpMethod: 'get',
+                identity: {
+                    accessKey: '',
+                    accountId: '',
+                    apiKey: '',
+                    apiKeyId: '',
+                    caller: '',
+                    clientCert: {
+                        clientCertPem: '',
+                        issuerDN: '',
+                        serialNumber: '',
+                        subjectDN: '',
+                        validity: { notAfter: '', notBefore: '' },
+                    },
+                    cognitoAuthenticationProvider: '',
+                    cognitoAuthenticationType: '',
+                    cognitoIdentityId: '',
+                    cognitoIdentityPoolId: '',
+                    principalOrgId: '',
+                    sourceIp: '',
+                    user: '',
+                    userAgent: '',
+                    userArn: '',
+                },
+                path: '/getPrices',
+                protocol: 'HTTP/1.1',
+                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+                requestTimeEpoch: 1428582896000,
+                resourceId: '123456',
+                resourcePath: '/getPrices',
+                stage: 'dev',
+            },
+            resource: '',
+            stageVariables: {},
+        };
+        const result: APIGatewayProxyResult = await lambdaHandler(event);
+    });
+});
